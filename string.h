4d9f913e289ab25157572ae7e272ce1e961f2073
Hadrian Węgrzynowski <hadrianw@users.noreply.github.com>
2017-03-20 19:53:15 +0100

    Add string_replace_multi to header.

--- string.h
+++ string.h
@@ -13,4 +13,5 @@ string_t *string_alloc(uint16_t size);
 string_t *string_realloc(string_t *str, uint16_t size);
 
 string_t *string_resize(string_t **pstr, uint16_t sub_off, uint16_t sub_len, uint16_t new_sub_len);
+string_t *string_replace_multi(string_t **pstr, uint16_t sub_off, uint16_t sub_len, rawbuf_t *mod, uint32_t nmod);
 string_t *string_replace(string_t **pstr, uint16_t sub_off, uint16_t sub_len, char *buf, uint16_t len);
